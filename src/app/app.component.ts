b14686c737cafdec4156a71fa5a9230ae6eec896
UTKARSH SHARMA <utkarshsharma@UTKARSHs-MacBook-Pro.local>
2023-05-25 23:24:59 +0530

    delete user data functionality updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,7 @@ export class AppComponent {
   tuser : any ;
 
   showUpdateForm : boolean=false;
+  showDeleteForm : boolean=false ;
 
   showTable: boolean= false;
   Users : any= [
@@ -82,6 +83,7 @@ export class AppComponent {
       
     // this.showRegister = !this.showRegister ;
     this.showRegister = false ;
+    this.showDeleteForm= false ;
     this.getapi();
     
    }
@@ -115,13 +117,15 @@ export class AppComponent {
   });
     
     this.showShowUsers = !this.showRegister ;
+    this.showRegister = false ;
+    alert(" Registered succesfully");
    }
 
   clicksignin()
   {
      this.showTable = false;
      this.showRegister =true ;
-
+     this.showDeleteForm= false;
   }
 
   // clickregister()
@@ -186,12 +190,47 @@ export class AppComponent {
         });
           
       console.log(this.up_id);
-
+      this.showUpdateForm = false;
+      this.showRegister = false;
+      this.showShowUsers =true ;
+      this.showTable =false ;
+      alert(" Updated succesfully");
    }
    deleteuserdata(tuser : any)
    {
-    console.log("deleted");
+          console.log("updated");
+          this.showUpdateForm = false;
+           this.showDeleteForm = true ;
+          this.showRegister = false;
+          this.showShowUsers =true ;
+          this.showTable =false ;
+          
+
+          this.up_id = tuser.id ;
+          this.up_name     =  tuser.name  ;
+          this.up_dob =   tuser.dob  ;
+       this.up_email    =   tuser.email  ;
+          this.up_school=   tuser.school  ; 
+           this.up_college =   tuser.college  ;
+       
    }
+   onClickDelete = async () => 
+   {
+    const response = await fetch(`http://localhost:3000/users/${this.up_id}`,{
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+  
+    });
+      
+  console.log(this.up_id);
+  this.showShowUsers =true ;
+  this.showTable =false ;
+  this.showDeleteForm =false ;
+  alert(" Deleted succesfully");
+   }
+     
 }
 
 
